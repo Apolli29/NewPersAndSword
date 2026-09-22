3678ff16c51bd0c457366a8d182d66c295748da5
234922 <234922@KB37-118-C01>
2026-09-22 10:52:54 +0300

    добавлен лук

---
+++ ChestOrMonster/Model/Item/Bow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChestOrMonster.Model.Item
+{
+    public class Bow : Weapon
+    {
+        public int Accuracy;
+
+        public Bow(string name, int damage, int accuracy)
+        : base(name, damage)
+        {
+            Accuracy = accuracy;
+        }
+
+        public bool Shoot()
+        {
+            Random random = new Random();
+            return random.Next(1, 101) <= Accuracy;
+        }
+    }
+
+}
